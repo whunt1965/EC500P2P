3bef30ab751a40e63e9ccde1caa89240051309ca
Wiley Hunt <johnhunt@MacBook-Pro.local>
2021-04-13 12:08:59 -0400

    Update server logic

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# EC500P2P
+# EC500_P2P_Chat
+
+| Collaborators               |
+| --------------------------- |
+| Ben Leone                   |
+| Wiley Hunt                  |
+| Noah Spahn                  |
+| Purvis Amin                 |
+
+
+## MVP
+
+- Register yourself in the app to become discoverable to other users.
+- Send a message to those you want to connect with to establish a session.
+- For users who are offline, sending a message stores the message locally to be sent when other user comes online.
+- All passwords and messages are hashed to be decrypted by other user.
+
+## Setup
+
+- Clone and `cd` into repository
+- `python3 -m venv venv`
+- `source venv/bin/activate`
+- `pip3 install -r requirements.txt`
+- Run `python3 client.py` 
+- Enter the host IP and port number of other client and chat!
+
+## Modules
+
+- Server.py
+    - Our remote server (hosted on GCP) which keeps track of users and IPs!
+- Client.py
+    - Our client application which enables a user to log-in, view other user IP's on the server, and engage in a Peer-to-Peer chat (over UDP)
+
+## Technologies Used
+
+- Application: Python (using the Twisted library)
+- Database: SQLite
--- src/server.py
+++ src/server.py
@@ -2,29 +2,37 @@ from twisted.internet.protocol import DatagramProtocol
 from twisted.internet import reactor
 from random import randint
 
-serverdata = dict()
-
 class Server(DatagramProtocol):
 
     def __init__(self):
-        self.clients = set()
+        self.clients = dict()
 
     def datagramReceived(self, datagram: bytes, addr):
         datagram = datagram.decode("utf-8")
         if "ready" in datagram:
+
+            # Parse datagram into ready&name&password
             try:
                 ready, name, pw = datagram.split("&")
-                password = serverdata.get(name)
             except:
                 self.transport.write("Please enter a username and password!".encode("utf-8"), addr)
                 return
-            if password == None or password == pw:
-                serverdata[name] = pw
-                addresses = "\n".join([str(x) for x in self.clients])
-                self.transport.write(addresses.encode("utf-8"), addr)
-                self.clients.add(name + str(addr))
+
+            # Check id name is already registered
+            if name in self.clients.keys():
+                data = self.clients.get(name) # Get array [password, IP]
+                password = data[0]
+                if password == pw: #Password matches
+                    self.serverdata[name] = [pw, addr] # Add pw and IP to server datastore
+                    addresses = "\n".join([str(x) + ": " + str(self.clients.get(x)[1]) for x in self.clients.keys()])
+                    self.transport.write(addresses.encode("utf-8"), addr)
+                else:
+                    self.transport.write("Invalid Password for this username".encode("utf-8"), addr)
+            # If name is not registered, add it
             else:
-                self.transport.write("Invalid Password!".encode("utf-8"), addr)
+                self.serverdata[name] = [pw, addr]  # Add pw and IP to server datastore
+                addresses = "\n".join([str(x) + ": " + str(self.clients.get(x)[1]) for x in self.clients.keys()])
+                self.transport.write(addresses.encode("utf-8"), addr)
 
 if __name__ == '__main__':
     reactor.listenUDP(9999, Server())
